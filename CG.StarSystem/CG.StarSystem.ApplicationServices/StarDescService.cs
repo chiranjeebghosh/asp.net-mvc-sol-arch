6637d19bacd0c0d81052ab5806f182d5d745a8f0
chiranjeebghosh <chiranjeebghosh@users.noreply.github.com>
2017-08-05 04:22:22 +0530

    added dispose
    added dispose

--- CG.StarSystem/CG.StarSystem.ApplicationServices/StarDescService.cs
+++ CG.StarSystem/CG.StarSystem.ApplicationServices/StarDescService.cs
@@ -61,29 +61,18 @@ namespace CG.StarSystem.ApplicationServices
             {
                 if (disposing)
                 {
-                    // TODO: dispose managed state (managed objects).
+                    _StarDescRepository.Dispose();
                 }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // TODO: set large fields to null.
 
                 disposedValue = true;
             }
         }
 
-        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        // ~StarDescService() {
-        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-        //   Dispose(false);
-        // }
-
         // This code added to correctly implement the disposable pattern.
         public void Dispose()
         {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
             Dispose(true);
-            // TODO: uncomment the following line if the finalizer is overridden above.
-            // GC.SuppressFinalize(this);
+            GC.SuppressFinalize(this);
         }
         #endregion
 
